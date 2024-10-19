3f43ffaa829254ef09ced45bbaa43dec98b112fe
cavoq <Davidstromberger99@gmx.de>
2024-10-19 22:02:39 +0200

    Proxy authentication

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Modern rotating TOR proxy.
 
 ## Usage
 
+**Create a `.env` file:**
+
+```bash
+USERNAME=<username>
+PASSWORD=<password>
+PRIVOXY_PORT=<port>
+TOR_PORT=<port>
+```
+
 **Build the container:**
 ```bash
 docker build -t proxy-rotator .
@@ -12,5 +21,11 @@ docker build -t proxy-rotator .
 **Run the container:**
 
 ```bash
-docker run -it -p 8080:8080 proxy-rotator
+docker run --env-file .env -it -p 8080:8080 proxy-rotator
+```
+
+**Test the proxy:**
+
+```bash
+curl -x http://localhost:8080 -U username:password http://httpbin.org/ip
 ```
--- mitmproxy.py
+++ mitmproxy.py
@@ -1,9 +1,20 @@
+import json
+import os
 from stem import Signal
 from stem.control import Controller
 from mitmproxy import http
+from dotenv import load_dotenv
 
-TOR_CONTROL_PORT = 9051
-PRIVOXY_PORT = 8118
+load_dotenv()
+
+TOR_CONTROL_PORT = os.environ.get("TOR_CONTROL_PORT", 9051)
+PRIVOXY_PORT = os.environ.get("PRIVOXY_PORT", 8118)
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+
+if not all([TOR_CONTROL_PORT, PRIVOXY_PORT, USERNAME, PASSWORD]):
+    raise ValueError(
+        "Missing environment variables: TOR_CONTROL_PORT, PRIVOXY_PORT, USERNAME, PASSWORD")
 
 
 def renew_tor_identity():
@@ -16,6 +27,29 @@ def renew_tor_identity():
         print(f"Error renewing Tor identity: {e}")
 
 
-def request(_: http.HTTPFlow):
+def request(flow: http.HTTPFlow):
     """Called for each HTTP request passing through mitmproxy."""
-    renew_tor_identity()
+    auth_header = flow.request.headers.get("Proxy-Authorization")
+
+    if not auth_header:
+        json_response = {
+            "error": "Proxy Authentication Required",
+            "message": "You must provide valid authentication credentials to access this proxy."
+        }
+        flow.response = http.Response.make(
+            407,
+            json.dumps(json_response).encode('utf-8'),
+            {
+                "Content-Type": "application/json",
+                "Proxy-Authenticate": 'Basic realm="proxy"'
+            }
+        )
+
+    method, credentials = auth_header.split(" ", 1)
+    if method.lower() == "basic":
+        import base64
+        decoded = base64.b64decode(credentials).decode("utf-8")
+        user, passwd = decoded.split(":", 1)
+
+        if user == USERNAME and passwd == PASSWORD:
+            renew_tor_identity()
